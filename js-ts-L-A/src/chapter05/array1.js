aafd9aabd71dd17b94f2f815cf7da9539cddc21e
Le Duc Anh <youremail@example.com>
2025-11-30 16:33:15 +0700

    update new code

---
+++ js-ts-L-A/src/chapter05/array1.js
@@ -0,0 +1,11 @@
+console.log("hello array with javascript");
+
+
+// Array :  
+
+const names = [ "Lê Đức Anh " , "Chó béo 2k6 " , "Bro Lê " , "19/04/2006" , "2k6", "9.8" ];
+
+const others = [ "Hello chó béo 2k6" , [ 1,  2 , 3 , 4 , 5  ] ];
+
+console.log(names);
+console.log(others);
