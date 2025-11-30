3adfe71556bf9a1e58d74ffe962b9d6dc0252994
Le Duc Anh <youremail@example.com>
2025-12-01 00:19:16 +0700

    update new code

---
+++ js-ts-L-A/src/chapter05/object42.js
@@ -0,0 +1,32 @@
+console.log("video 42 hello javascript with object");
+
+// object : 
+
+const scores = [10 , 4, 5, 6, 7];
+
+// key : value.
+
+const sv1 = {
+     name : "Lê Đức Anh",
+     age  : 19,
+     score : 10,
+     address : {
+       city : "TPHCM",
+       country : "Việt Nam" 
+        }
+}
+
+const sv2 = {
+     name : " Bro Lê ",
+     age  : 20,
+     score : 9,
+     address : {
+       city : "TPHCM",
+       country : "Việt Nam" 
+        }
+}
+
+const sinhvien = [sv1 , sv2];
+
+console.log("scores : ", scores);
+console.log("Sinh viên : " , sinhvien);
