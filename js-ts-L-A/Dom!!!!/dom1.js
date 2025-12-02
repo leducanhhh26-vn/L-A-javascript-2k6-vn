23e4fd68d5203f1163fedf9a4d8d80c6b9ee8c80
Le Duc Anh <youremail@example.com>
2025-12-02 11:51:11 +0700

    update

---
+++ js-ts-L-A/Dom!!!!/dom1.js
@@ -0,0 +1,11 @@
+console.log("hello dom in HTML");
+
+const element = document.getElementById("username");
+
+const mydiv  = document.querySelector(".hoidanit");
+
+const allmydiv = document.querySelectorAll(".hoidanit");
+
+ // console.log(element);
+
+console.log(allmydiv);
