244aec53b1aba6288ec71b527c7a79d262d52c8e
Le Duc Anh <youremail@example.com>
2025-12-02 12:48:58 +0700

    update new code

---
+++ js-ts-L-A/Dom!!!!/dom3.js
@@ -0,0 +1,11 @@
+console.log("hello video 51");
+
+const element = document.getElementById("mybtn");
+
+const handleclickbutton = () => {
+    console.log("you click a button ");
+}
+
+element.addEventListener("click", handleclickbutton)
+
+console.log(element);
